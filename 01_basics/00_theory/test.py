60c652e070028b84478c5070bf4592663ae628d5
Andrey <andrey@example.com>
2026-07-21 22:26:32 +0400

    Провел тисты по кортежам и спискам

--- 01_basics/00_theory/test.py
+++ 01_basics/00_theory/test.py
@@ -1,4 +1,3 @@
-numbers = []
-for i in range(10):
-    numbers.append(int(input()))
-print(numbers)
+countries_and_capitals = {"Россия": "Москва", "США": "Вашингтон", "Франция": "Париж"}
+countries_and_capitals["Сербия"] = "Белград"
+print(countries_and_capitals)
